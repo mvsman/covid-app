c9b22078b6601d54917ce49ce517093baba66dd0
Vladimir <mvsman8@gmail.com>
2021-07-21 11:25:03 +0500

    added Links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,18 @@
+import { Link } from 'react-router-dom';
+
 function Nav() {
   return (
     <nav className="nav">
       <ul className="nav__list">
-        <li className="nav__item">Description</li>
-        <li className="nav__item">World</li>
-        <li className="nav__item">Country</li>
+        <li className="nav__item">
+          <Link to="/">Description</Link>
+        </li>
+        <li className="nav__item">
+          <Link to="/world">World</Link>
+        </li>
+        <li className="nav__item">
+          <Link to="/country">Country</Link>
+        </li>
       </ul>
     </nav>
   );
