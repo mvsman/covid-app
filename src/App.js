d5be949207c37a03b8336f28767da82d0897cb91
Vladimir <mvsman8@gmail.com>
2021-07-21 11:26:19 +0500

    added routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import axios from 'axios';
 import {useState, useEffect} from 'react';
+import { Route } from 'react-router-dom';
 // import Data from './components/Data'
 import Header from './components/Header';
 import Nav from './components/Nav';
-// import Description from './components/Description';
+import Description from './components/Description';
 import World from './components/World';
 import Country from './components/Country';
 
 import './App.css';
-// import { Route, Router, Switch } from 'react-router-dom';
 
 function App() {
   const [state, setState] = useState([])
-  // const [inputValue, setInputValue] = useState('')
 
   useEffect(() => {
     const API = `https://covid-api.mmediagroup.fr/v1/cases`
@@ -21,27 +20,15 @@ function App() {
     })
   }, [])
 
-  // if(inputValue) {
-    
-  // }
-
   return (
     <div className="App">
       <Header />
       <Nav />
       <div className="container">
-      {/* <input type="text" value={inputValue} onChange={e => setInputValue(e.target.value)}/> */}
         <div className="page">
-          {/* <Router>
-            <Switch>
-              <Route exact path="/" component={<Description items={state} />} />
-              <Route exact path="/world" component={<World items={state} />} />
-              <Route exact path="/country" component={<Country items={state} /> } />
-            </Switch>
-          </Router> */}
-          {/* <Description items={state} /> */}
-          <World items={state} />
-          <Country items={state} />
+          <Route path="/" render={() => <Description items={state} />} exact />
+          <Route path="/world" render={() => <World items={state} />} exact />
+          <Route path="/country" render={() => <Country items={state} />} exact />
           {/* <Data items={state} /> */}
         </div>
       </div>
