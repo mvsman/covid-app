649b5c49e4aac87a1b561b0920f22fe691d6e97b
Vladimir <mvsman8@gmail.com>
2021-07-26 17:05:10 +0500

    .

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename="/covid-app">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>,
